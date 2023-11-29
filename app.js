0bf3cdb027d325ed5ec091148367e7d0d8de95e0
khyun-0123 <128883244+khyun-0123@users.noreply.github.com>
2023-11-29 09:32:40 +0900

    edit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 
 
 
-const mysql = require('mysql2')
+const mysql = require('mysql')
 const connection = mysql.createConnection(process.env.DATABASE_URL)
 console.log('Connected to PlanetScale!')
 
